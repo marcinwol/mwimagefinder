825a4d317598f375123987808ee6c688260f3c14
marcinwol <wolskint@o2.pl>
2015-02-09 06:29:27 +0800

    file_types vector added.

--- app/main.cpp
+++ app/main.cpp
@@ -24,7 +24,7 @@
 
 
 using namespace std;
-
+using namespace boost::filesystem;
 
 
 
@@ -38,17 +38,26 @@ int main(int ac, char* av[])
         return 1;
     }
 
-    string in_dir    = po.get<string>("in-dir");
-    string out_dir   = po.get<string>("out-dir");
-    string file_type = po.get<string>("file-type");
-    string path_file = po.get<string>("path-file");
-    string out_csv   = po.get<string>("csv-file");
-    bool verbose     = po.get<bool>("verbose");
+    bool verbose        {po.get<bool>("verbose")};
+    path in_dir         {po.get<string>("in-dir")};
+    path out_dir        {po.get<string>("out-dir")};
+    path path_file      {po.get<string>("path-file")};
+    path out_csv        {po.get<string>("csv-file")};
+    string file_type    {po.get<string>("file-type")};
 
+    vector<string> file_types {mw::split(file_type, ',')};
 
+    if (!exists(in_dir)) {
+         in_dir = path{current_path()};
+    }
 
+    if (out_csv.empty()) {
+         out_csv = path{in_dir / path("found_files.csv")};
+    }
 
     cout << "In-dir: " << in_dir << endl;
+    cout << "out_csv: " << out_csv << endl;
+    mw::print_iterable(file_types);
 
 
     return 0;
