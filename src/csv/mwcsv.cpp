58e7c5ff37c56b4f2ceaa5650f49a9980fe811e0
marcinwol <wolskint@o2.pl>
2015-02-17 18:20:54 +0800

    csv/mwcsv library initilized.

---
+++ src/csv/mwcsv.cpp
@@ -0,0 +1,19 @@
+#include "mwcsv.h"
+
+
+
+namespace mw {
+
+  mwcsv::mwcsv()
+  {
+
+    cout <<"MwCSV constructor"<< endl;
+  }
+
+  mwcsv::~mwcsv()
+  {
+
+  }
+
+}
+
---
+++ src/csv/mwcsv.h
@@ -0,0 +1,22 @@
+#ifndef MWCSV_H
+#define MWCSV_H
+
+
+#include<iostream>
+#include<vector>
+
+
+namespace mw {
+
+  using namespace std;
+
+  class mwcsv
+  {
+  public:
+    mwcsv();
+    ~mwcsv();
+  };
+
+}
+
+#endif // MWCSV_H
